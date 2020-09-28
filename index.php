5df1663b980f5dde9c6783fa23e745dcf91405a7
sachini <sachini.mandakini123@gmail.com>
2020-09-28 15:38:05 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript">src = "index.html" </script> 
+	<title></title>
+</head>
+<body>
+
+<?php  
+$x=15;  
+$y=30;  
+$z=$x+$y;  
+echo "Sum: ",$z;  
+?>  
+
+</body>
+</html>
---
+++ validate.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	
+<title> JavaScript email validation </title>
+
+<script src="email-validate.js"> </script>
+
+	<link rel='stylesheet' href='email-valid-style.css' type='text/css' />
+</head>
+
+<body onload='Desktop.ICT010.Que3.email-valid-style.focus()'>
+	<div class="mail">
+	<h2> Enter email to Validate </h2>
+	
+	
+<form name="email-valid-style" action="#">
+	
+	<ul>
+		<li>  <input type='text' name='text1'/> </li>
+		<li> </li>
+		<li class="Validate"><input type="submit" name="Validate" value="Validate" onclick="ValidateEmail(Desktop.ICT010.Que.email-valid-style.css)"/> 
+		</li>
+		<li> </li>
+	</ul>
+</form>
+	</div>
+
+<script src="email-validate.js"> </script>
+	</body>
+</html>
+
+
+
+
